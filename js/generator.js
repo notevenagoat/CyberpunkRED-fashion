b65381041c405e429131244167d1f5f9eaa9371c
notevenagoat <itinoco@pacificstream.com.au>
2021-06-17 23:06:04 +1000

    ads a weeke of random fasion without accessories on button press

--- js/generator.js
+++ js/generator.js
@@ -1,13 +1,45 @@
-function loadTableData(items) {
-    const table = document.getElementById("testBody");
-    items.forEach( item => {
-      let row = table.insertRow();
-      let date = row.insertCell(0);
-      date.innerHTML = item.date;
-      let name = row.insertCell(1);
-      name.innerHTML = item.name;
-    });
+
+const clothingStyles = ['Bag Lady Chic', 'Gang Colors', 'Generic Chic', 'Bohemian', 'Leisurewear', 'Nomad Leathers', 'Asia Pop', 'Urban Flash', 'Businesswear'];
+
+let getrandomStyle = () => {
+  let randomStyle = clothingStyles[Math.floor(Math.random() * 8)]
+  return randomStyle;
+  
   }
-  loadTableData(items1);
-  loadTableData(items2);
-  loadTableData([]);
+
+  const fashionTable = document.getElementById("fashionTable");
+  
+  let getSeason = () => {
+    let currentSeason=[];
+    for (i=0;i<7;i++) {
+      let currentStyle = getrandomStyle();
+      currentSeason.push(currentStyle);
+    }
+    return (currentSeason);
+  }
+  let htmlFashionTable = [];
+
+  let putSeason = (week) => {
+     let htmlSeason = getSeason();
+       tableContent = `
+    <tr>
+        <th scope="row">${week}</th>
+        <td>${htmlSeason[0]}</td>
+        <td>${htmlSeason[1]}</td>
+        <td>${htmlSeason[2]}</td>
+        <td>${htmlSeason[3]}</td>
+        <td>${htmlSeason[4]}</td>
+        <td>${htmlSeason[5]}</td>
+        <td>${htmlSeason[6]}</td>
+      </tr>`
+      htmlFashionTable.push(tableContent);
+      return htmlFashionTable;
+  }
+
+let btn = document.querySelector("#add-season");
+let week = 1;
+btn.addEventListener('click', function (event) {git
+  finalHtml=putSeason(week);
+  fashionTable.innerHTML = finalHtml.join("\n");
+  week++;
+  });
